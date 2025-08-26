5edb4c09054a77503acb734c0bf835e0dd14135f
Justin C Miller <justin@justin.cm>
2025-08-26 01:19:16 -0700

    Neovide font and copypaste updates

--- lua/justin/neovide.lua
+++ lua/justin/neovide.lua
@@ -1,4 +1,5 @@
-vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h13"
+vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h11"
+vim.g.neovide_scale_factor = 0.95
 
 vim.g.neovide_text_gamma = 0.5
 vim.g.neovide_text_contrast = 0.5
@@ -12,3 +13,8 @@ vim.g.neovide_cursor_animation_length = 0.08
 
 vim.g.neovide_hide_mouse_when_typing = true
 
+vim.keymap.set('n', '<C-c>', '"+y')
+vim.keymap.set('v', '<C-c>', '"+y')
+vim.keymap.set('n', '<C-v>', '"+P')
+vim.keymap.set('v', '<C-v>', '"+P')
+vim.keymap.set('i', '<C-v>', '<C-o>"+P')
